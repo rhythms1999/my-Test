b1ef2a4234d03ed6363741e023dabcb74f8601a2
rhythms1999 <1527325300@qq.com>
2024-04-14 19:57:46 +0800

    第一题-哈希表解法

--- src/Question1/Arrknowledge.java
+++ src/Question1/Arrknowledge.java
@@ -1,7 +1,5 @@
 package Question1;
 
-import java.util.Arrays;
-
 /*给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
@@ -23,23 +21,29 @@ import java.util.Arrays;
 
  */
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Arrknowledge {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         int[] nums = {2, 7, 11, 15};
         int target = 26;
-        System.out.println(Arrays.toString(Solution.twoSum(nums, target)));
+        System.out.println(Arrays.toString(Solution2.twoSum(nums, target)));
     }
-    static class Solution {
-        public static int[] twoSum(int[] nums, int target) {
-            for (int i = 0; i < nums.length - 1; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[i] + nums[j] == target) {
-                        return new int[]{i, j};
-                    }
+    static class Solution2 {
+        public static int[] twoSum(int[] nums, int target) throws IllegalAccessException {
+            int length = nums.length;
+            Map<Integer,Integer> hashMap = new HashMap<>(length-1);
+            hashMap.put(nums[0],0);// 第一个元素之前一定没有元素与它对应，直接存进哈希表
+
+            for (int i = 1; i < length ; i++) {
+                int another = target-nums[i];
+                if (hashMap.containsKey(another)){
+                    return new int[]{i,hashMap.get(another)};
                 }
+                hashMap.put(nums[i],i);
             }
-            return null;
+            throw new IllegalAccessException("No two sum solution");
         }
     }
 }
