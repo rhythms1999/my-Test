326be6f3c09f51820fedd4d55ebdaaaf90db9154
rhythms1999 <1527325300@qq.com>
2024-04-14 19:39:45 +0800

    第一题-Solution类

--- src/Question1/Arrknowledge.java
+++ src/Question1/Arrknowledge.java
@@ -28,19 +28,19 @@ public class Arrknowledge {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 26;
-        System.out.println(Arrays.toString(twoSum(nums, target)));
+        System.out.println(Arrays.toString(Solution.twoSum(nums, target)));
     }
-
-    public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+    static class Solution {
+        public static int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
                 }
             }
+            return null;
         }
-        return null; // 或者return new int[]{-1, -1};返回一个标识数组，表示未找到符合条件的元素
     }
 }
-
 
